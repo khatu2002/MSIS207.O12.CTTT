043a16c7b259862cf62b638b38a3b92838db4ef9
khatu2002 <20522096@gm.uit.edu.vn>
2023-11-23 21:04:24 +0700

    Update login.js

--- Basic Javascript/Homework 2.3/js/login.js
+++ Basic Javascript/Homework 2.3/js/login.js
@@ -1,6 +1,6 @@
 var user = document.getElementById("user");
 var password = document.getElementById("pwd");
-var eyeIcon = document.getElementById("eye-con");
+var eyeIcon = document.getElementById("eye-icon");
 var eye = document.getElementById("hide-eye");
 var eyeSlash = document.getElementById("hide-eye-slash");
 var login = document.getElementById("login");
@@ -9,27 +9,53 @@ var hello = document.getElementById("hello");
 eyeIcon.onclick = function () {
     if (password.type == "password") {
         password.type = "text";
-        eye.style.display = "block";
-        eyeSlash.style.display = "none";
-
+        eye.style.display = "none";
+        eyeSlash.style.display = "block";
     } else {
         password.type = "password";
-        eye.style.display = "none";
+        eye.style.display = "block";
         eyeSlash.style.display = "none";
     }
+}
+
+function validateCredentials(username, password) {
+    // Check if the surname has extra spaces or contains special characters
+    const surnameRegex = /^[a-zA-Z\s']*$/;
+    if (!surnameRegex.test(username.trim())) {
+        alert("Invalid Username. Surname should only contain letters and spaces.");
+        return false;
+    }
+
+    // Check if the password contains special characters other than '@'
+    const passwordRegex = /^[^\s@]+$/;
+    if (!passwordRegex.test(password)) {
+        alert("Invalid Password. Password should not contain special characters other than '@'.");
+        return false;
+    }
+
+    // If both checks pass, return true indicating valid credentials
+    return true;
 }
 
 function loginClick() {
     var name = document.querySelector("#user-name");
     login.style.display = 'none';
     hello.style.display = 'block';
-    if (user.value != '') {
-        name.innerHTML = user.value;
-    }
-    else {
-        name.innerHTML = 'Not enter username';
+
+    // Validate entered credentials
+    if (validateCredentials(user.value, password.value)) {
+        if (user.value != '') {
+            name.innerHTML = user.value;
+        } else {
+            name.innerHTML = 'Not enter username';
+        }
+    } else {
+        // Invalid credentials, show alert and reset password field
+        alert("Invalid Name or Password.");
+        password.value = '';
     }
 }
+
 function logoutClick() {
     login.style.display = 'block';
     hello.style.display = 'none';
