4aef610a37f7cbd2148c9b9a4dbce27e9c8178bb
khatu2002 <20522096@gm.uit.edu.vn>
2023-11-24 21:10:00 +0700

    commit

--- Basic Javascript/Homework 2.1/script.js
+++ Basic Javascript/Homework 2.1/script.js
@@ -1,17 +1,16 @@
-function playDrumSound() {
-    var audio = document.getElementById('drumSound');
-    var drumButton = document.querySelector('.w.drum');
+function playDrumSound(key) {
+    var audio = document.getElementById('drumSound-' + key);
+    var drumButton = document.querySelector('.' + key + '.drum');
     drumButton.classList.add('blinking');
     audio.play();
     setTimeout(function () {
         drumButton.classList.remove('blinking');
     }, 2000);
-
-
 }
 
 document.addEventListener('keypress', function (event) {
-    if (event.key === 'w') {
-        playDrumSound();
+    var key = event.key.toLowerCase();
+    if (key === 'w' || key === 'a' || key === 's' || key === 'd' || key === 'j' || key === 'k' || key === 'l') {
+        playDrumSound(key);
     }
 });
